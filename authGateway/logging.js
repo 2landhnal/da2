e25a0df45ebc6c2ccbf683d6b43a4ae23ca2f92f
Cao <nguyencb@hapiga.net>
2024-12-26 12:34:39 +0700

    fix merrge

--- authGateway/logging.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import morgan from 'morgan';
-
-const setupLogging = (app) => {
-    app.use(morgan('combined'));
-};
-
-export { setupLogging };
--- authService/routes/teacherRequired/index.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-import express from 'express';
-import accountController from '../../controllers/accountController.js';
-import tokenController from '../../controllers/tokenController.js';
-const router = express.Router();
-
-export default router;
